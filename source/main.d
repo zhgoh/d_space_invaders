4b86e3180aa9e631e369cf2b0f535bfa86f3ab4e
zihe.goh <zihe.goh@gmail.com>
2018-07-07 10:16:33 +0800

    Finished part 1

--- source/main.d
+++ source/main.d
@@ -2,22 +2,33 @@ import derelict.glfw3.glfw3;
 import derelict.opengl;
 import std.experimental.logger;
 import std.stdio;
+import std.typecons;
+import std.string : format, fromStringz;
 
 version(Windows) string libName = "dlls\\glfw3.dll";
 else string libName = "glfw3.so";
 
 void main()
 {
+  // Using Derelict to load openGL/GLFW
   DerelictGL3.load();
   DerelictGLFW3.load("dlls\\glfw3.dll");
-  
+
+  // Setting error callbacks
+  glfwSetErrorCallback(&error_callback);
+
   if (!glfwInit())
   {
     fatal("glfw failed to init");
     return;
   }
+
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   
-  auto window = glfwCreateWindow(640, 480, "Game", null, null);
+  auto window = glfwCreateWindow(640, 480, "Space Invaders", null, null);
   if(window is null)
   {
       fatal("glfw failed to create window");
@@ -41,4 +52,12 @@ void main()
   
   glfwDestroyWindow(window);
   glfwTerminate();
+}
+
+extern(C) nothrow
+{
+	void error_callback(int error, const(char)* description)
+	{
+		throw new Error(format("Error: %s : %s", error, fromStringz(description)));
+  }
 }
